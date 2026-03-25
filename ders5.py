ce5413b19f798131496b7c0298da1419df48a65f
meltmkksl-gif <meltm3235@gmail.com>
2026-03-25 21:25:58 +0300

    examples

---
+++ ders5.py
@@ -0,0 +1,42 @@
+
+'''''
+filename = input("Enter filename: ")
+keyword = input("Enter keyword: ")
+
+count = 0
+
+with open(filename, "r") as f:
+    for line in f:
+        if keyword in line:
+            print(line.strip())
+            count += 1
+
+print("Total lines found:", count)
+
+with open("file.txt", "r") as f:
+    content = f.read()
+
+print("Total characters:", len(content))
+print("First 50 characters:", content[:50])
+
+domain = "@uludag.edu.tr"
+count = 0
+
+
+
+filename = input("Enter filename: ")
+keyword = input("Enter keyword: ")
+
+count = 0
+
+with open(filename, "r") as f:
+    for line in f:
+        if keyword in line:
+            print(line.strip())
+            count += 1
+'''
+with open("mel.txt", "r", encoding="utf-8") as f:
+    content = f.read()
+
+print("Total characters:", len(content))
+print("First 50 characters:", content[:50])
