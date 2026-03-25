d182ca871f939318e5f45eb47e133906ef0394a8
meltmkksl-gif <meltm3235@gmail.com>
2026-03-25 21:05:19 +0300

    examples

--- ders4.py
+++ ders4.py
@@ -26,10 +26,30 @@ for letter in word:
     new_word = letter + new_word
 
 print(new_word)
-'''
+
 
 fruit="banana"
 a=fruit.find("a")
 print(a)
 a=fruit.find("c")
 print(a)
+
+
+word = input("Enter a word: ")
+vowels = "aeiouAEIOU"
+count = 0
+
+for letter in word:
+    if letter in vowels:
+        count += 1
+
+print("Number of vowels:", count)
+
+first = input("Enter first name: ")
+last = input("Enter last name: ")
+
+email = first + "." + last + "@student.university.com"
+
+print("Email:", email)
+print("Length:", len(email))
+'''
